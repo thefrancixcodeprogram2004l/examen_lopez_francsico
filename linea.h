3ac53df10c5be0da3c8e2b3ccbb5cb6b7ec4c9ec
francisco <flopeze@est.ups.edu.ec>
2022-11-24 12:08:35 -0500

    commit incial

---
+++ linea.h
@@ -0,0 +1,16 @@
+#ifndef LINEA_H
+#define LINEA_H
+#include<iostream>
+#include<stack>
+using namespace std;
+class Linea
+{
+private:
+    float distancia;
+    float pendiente;
+    int Num_lineas;
+public:
+    Linea();
+};
+
+#endif // LINEA_H
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ *Nombre: FRancsico lopez
+ * cedula: 175105617-5
+ * correo: flopeze@est.ups.edu.ec
+ * fecha: 24-nov-2022
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Francisco Lopez\n";
+
+    return 0;
+}
